ac74b99c3e44a36db1d3542ae0b19cb7ab803ab4
kema1989 <86516029+kema1989@users.noreply.github.com>
2021-11-21 00:03:25 +0600

    Добавьте файлы проекта.

---
+++ M16U4-3.Tests/CalculatorTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace M16U4_3.Tests
+{
+    public class CalculatorTests
+    {
+        public Calculator calculator = new Calculator();
+
+        [Fact]
+        public void AdditionMustReturnCorrectValue()
+        {
+            Assert.Equal(399, calculator.Addition(7, 49, 343));
+        }
+
+        [Fact]
+        public void MultiplicationMustReturnCorrectValue()
+        {
+            Assert.Equal(24, calculator.Multiplication(2, 3, 4));
+        }
+    }
+}
---
+++ M16U4-3/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M16U4_3
+{
+    public class Calculator
+    {
+        public int Addition(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        public int Multiplication(int a, int b, int c)
+        {
+            return a * b * c;
+        }
+    }
+}
